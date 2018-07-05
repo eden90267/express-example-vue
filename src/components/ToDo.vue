d3a6bf1e0970c6eb95dbc33c6149affbacd3bbb2
smlsunxie <smlsun@gmail.com>
2018-07-05 16:39:09 +0800

    調整前端針對 completed 的變動，呼叫完成的 API

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="todo w3-container w3-center">
     <label>{{index + 1}}.- {{todo.title}}</label>
-    <input class="w3-check" type="checkbox" v-model="todo.completed">
+    <input class="w3-check" type="checkbox" v-model="todo.completed" @change="updateTodo">
     <hr>
   </div>
 </template>
@@ -16,6 +16,20 @@ export default {
     index: {
       type: Number,
       required: true
+    }
+  },
+  methods: {
+    async updateTodo(e) {
+      let id = this.todo.id;
+      let completed = e.target.checked;
+      let data = {
+        completed
+      }
+      let response = await fetch(`http://localhost:3000/api/task/${id}`, {
+        method: 'put',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(data)
+      });
     }
   }
 }
